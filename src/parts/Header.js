c4d3b8f17550ad84268710f2239b23d86beb11d2
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2022-04-25 11:40:11 +0700

    update code part Header

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,48 +1,160 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { RiMenu4Fill } from 'react-icons/ri';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      color: isActive ? '#7839F3' : '',
+    };
+  };
+
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+      header.classList.add('fixed-nav');
+    } else {
+      header.classList.remove('fixed-nav');
+    }
+  };
+
   return (
-    <header className='shadow-header'>
+    <header className='shadow-header absolute w-full'>
       <div className='container'>
-        <div className='flex items-center justify-between'>
+        <div className='relative flex items-center justify-between'>
           <div className='flex'>
-            <a href='#home' className='block py-7'>
-              <img src='/images/logo.png' alt='' className='h-10' />
-            </a>
+            <Link to='/' className='flex items-center py-5 lg:py-7'>
+              <img src='/images/logo.svg' alt='' className='h-6 lg:h-9' />
+              <span className='pl-2.5 text-lg font-medium text-slate-900 lg:text-2.5xl'>
+                laracamp
+              </span>
+            </Link>
             <div className='flex items-center'>
-              <nav>
-                <ul className='flex lg:space-x-7 lg:pl-12'>
+              <nav
+                className={[
+                  'nav-menu lg:static lg:block lg:rounded-none lg:bg-transparent lg:shadow-none',
+                  click
+                    ? 'absolute top-16 right-4 w-full max-w-[15.625rem] rounded bg-white py-5 px-5 shadow-lg'
+                    : 'hidden',
+                ].join(' ')}
+              >
+                {/* desktop */}
+                <ul className='hidden lg:flex lg:space-x-7 lg:pl-12'>
                   <li>
-                    <a href='#' className='text-navbar'>
+                    <NavLink
+                      to='/program'
+                      className='text-navbar'
+                      style={navLinkStyles}
+                    >
                       Program
-                    </a>
+                    </NavLink>
                   </li>
                   <li>
-                    <a href='#' className='text-navbar'>
+                    <NavLink
+                      to='/mentor'
+                      className='text-navbar'
+                      style={navLinkStyles}
+                    >
                       Mentor
-                    </a>
+                    </NavLink>
                   </li>
                   <li>
-                    <a href='#' className='text-navbar'>
+                    <NavLink
+                      to='/pricing'
+                      className='text-navbar'
+                      style={navLinkStyles}
+                    >
                       Pricing
-                    </a>
+                    </NavLink>
                   </li>
                   <li>
-                    <a href='#' className='text-navbar'>
+                    <NavLink
+                      to='/business'
+                      className='text-navbar'
+                      style={navLinkStyles}
+                    >
                       Business
+                    </NavLink>
+                  </li>
+                </ul>
+
+                {/* mobile */}
+                <ul className='block space-y-5 lg:hidden'>
+                  <li>
+                    <NavLink
+                      to='/program'
+                      className='text-navbar'
+                      style={navLinkStyles}
+                    >
+                      Program
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to='/mentor'
+                      className='text-navbar'
+                      style={navLinkStyles}
+                    >
+                      Mentor
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to='/pricing'
+                      className='text-navbar'
+                      style={navLinkStyles}
+                    >
+                      Pricing
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to='/business'
+                      className='text-navbar'
+                      style={navLinkStyles}
+                    >
+                      Business
+                    </NavLink>
+                  </li>
+                  <li className='flex flex-wrap space-y-3 text-center'>
+                    <a href='#' className='btn btn-secondary w-full'>
+                      Sign In
+                    </a>
+                    <a href='#' className='btn btn-primary w-full'>
+                      Sign Up
                     </a>
                   </li>
                 </ul>
               </nav>
             </div>
           </div>
-          <div className='flex lg:space-x-5'>
+
+          {/* Desktop */}
+          <div className='hidden lg:flex lg:space-x-5'>
             <a href='#' className='btn btn-secondary'>
               Sign In
             </a>
             <a href='#' className='btn btn-primary'>
               Sign Up
             </a>
+          </div>
+          <div className='hamburger lg:hidden'>
+            <button
+              className='rounded-md bg-dark px-1.5 py-1.5 transition duration-500'
+              onClick={handleClick}
+            >
+              {click ? (
+                <FaTimes size={20} style={{ color: 'white' }} />
+              ) : (
+                <RiMenu4Fill size={20} style={{ color: 'white' }} />
+              )}
+            </button>
           </div>
         </div>
       </div>
